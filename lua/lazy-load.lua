d328b5caae5a82480e47360b41706c6ca44ab8d4
xezo360hye <xezo360hye@gmail.com>
2025-08-06 17:41:26 +0300

    fixed indentation

--- lua/lazy-load.lua
+++ lua/lazy-load.lua
@@ -1,28 +1,28 @@
 local M = {}
 
 function M.setup(module, opts)
-  local instance = nil
+   local instance = nil
 
-  local function setup()
-    if instance == nil then
-      instance = require(module)
-      if instance.setup then
-        instance.setup(opts or {})
+   local function setup()
+      if instance == nil then
+         instance = require(module)
+         if instance.setup then
+            instance.setup(opts or {})
+         end
       end
-    end
-  end
+   end
 
-  return setmetatable({}, {
-    __index = function(_, key)
-      setup()
-      return instance[key]
-    end,
+   return setmetatable({}, {
+      __index = function(_, key)
+         setup()
+         return instance[key]
+      end,
 
-    __call = function(_, ...)
-      setup()
-      return instance(...)
-    end,
-  })
+      __call = function(_, ...)
+         setup()
+         return instance(...)
+      end,
+   })
 end
 
 return M
