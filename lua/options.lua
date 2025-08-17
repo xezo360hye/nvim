00ce68eaf04df2472011b24075490497a6bde2ba
xezo360hye <xezo360hye@gmail.com>
2025-08-18 02:58:05 +0300

    updated options

--- lua/options.lua
+++ lua/options.lua
@@ -6,11 +6,12 @@ vim.o.wrap = false
 vim.o.undofile = true
 vim.g.mapleader = " "
 vim.o.winborder = "single"
-vim.o.completeopt = "menu,popup,noselect"
 vim.o.signcolumn = "yes"
 vim.o.foldmethod = "expr"
 vim.o.foldlevel = 99
 vim.o.inccommand = "split"
+vim.o.swapfile=false
+vim.o.updatetime=250
 
 vim.api.nvim_create_autocmd("TextYankPost", {
    callback = function()
