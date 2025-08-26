87b479246a1724e526028f23ef6052d5d331f035
xezo360hye <xezo360hye@gmail.com>
2025-08-26 13:19:56 +0300

    added nvim-treesitter-context

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,6 @@
 vim.pack.add({
    "https://github.com/nvim-treesitter/nvim-treesitter",
+   "https://github.com/nvim-treesitter/nvim-treesitter-context",
    "https://github.com/neovim/nvim-lspconfig",
    "https://github.com/nvim-lua/plenary.nvim",
 
